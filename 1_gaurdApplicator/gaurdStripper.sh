005ceb66965a888aad860e1dd282cd012e217dd9
Dylan95 <dylan.gundlach@gmail.com>
2016-09-12 11:31:11 -0400

    no message

--- 1_gaurdApplicator/gaurdStripper.sh
+++
@@ -1,9 +0,0 @@
-for f in $(find ./include/ -type f); do 
-	fileStr=$(cat "$f");
-	#remove ifndef/define
-	gaurdRemoved=$(tail -n +2 "$f"); 
-	#remove endif
-	strippedText=$(tail n +1 "$f"); 
-	echo "$strippedText" > $f; 
-done
-
